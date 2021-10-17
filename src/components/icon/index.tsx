079408621a500155fd8f05489b324f115e0b91c9
spest3122 <spest3122@gmail.com>
2021-10-17 11:38:14 +0800

    今日事項 1. 增加icon 型別判定

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,3 +1,3 @@
-export default ({ src }) => {
-    return <img src={src} alt="icon" width="25" height="25" />
+export default (data: { src: string }) => {
+    return <img src={data.src} alt="icon" width="25" height="25" />
 }
